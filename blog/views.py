89a8af5a6b157a8e31d1707604d13f2f0fc5b776
aslon1213 <hamidovaslon1@gmail.com>
2022-05-19 11:16:44 +0500

    simple blog

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+# blog/views.py
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = 'post_detail.html'
+
