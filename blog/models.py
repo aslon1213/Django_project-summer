a0565d82905e623cccf3c378922f0043513aa8a1
aslon1213 <hamidovaslon1@gmail.com>
2022-05-19 20:21:46 +0500

    CRUD(create-read-update-delete) posts is implemented

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 # blog/models.py
 from django.db import models
+from django.urls import reverse # new
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -7,5 +8,11 @@ class Post(models.Model):
     on_delete=models.CASCADE,
     )
     body = models.TextField()
+
+
     def __str__(self):
         return self.title
+
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView# new
 from .models import Post
-
-
+from django.urls import reverse_lazy
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
-
-class BlogDetailView(DetailView): # new
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
 
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'delete_blog.html'
+    success_url = reverse_lazy('home')
